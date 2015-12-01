fff452817d64a777795100b3007fd733341dbcbf
Grey <guohui.luo@interfocus.cn>
2015-12-01 19:00:58 +0800

    init

---
+++ GLDataStore-CoreData/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  GLDataStore-CoreData
+//
+//  Created by hyq on 15/12/1.
+//  Copyright © 2015年 Grey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;//数据上下文
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;//数据模型
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;//数据持久化助理
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
